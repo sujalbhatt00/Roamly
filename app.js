1502ac3dc81bddecfa193a566143641f33b2ed2b
Sujal Bhatt <sujalbhatt500@gmail.com>
2025-09-21 01:31:39 +0530

    added joi for schema validation

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ const ejsMate=require("ejs-mate")
 app.engine("ejs",ejsMate)
 const wrapAsync=require("./utils/wrapAsync")
 const ExpressError=require("./utils/ExpressError");
+const{listingSchema}=require("./schema.js")
+
 
 const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
 main().then(() =>{
@@ -30,7 +32,19 @@ async function main() {
 app.get("/",  wrapAsync(async(req,res)=>{
      const allListing= await Listing.find({});
  res.render("listings/index.ejs",{allListing});
-}))
+}));
+
+ const validateListing=(req,res,next)=>{
+    let{error}=listingSchema.validate(req.body);
+    
+    if(error){
+        let errMsg=error.details.map((el)=> el.message).join(",")
+        throw new ExpressError(400,errMsg);
+    }
+    else{
+        next();
+    }
+ }
 //new route
  app.get("/listings/new",(req,res)=>{
     res.render("listings/new.ejs")
@@ -45,9 +59,9 @@ app.get("/listing", wrapAsync(async(req,res)=>{
     const listing=await Listing.findById(id);
     res.render("listings/show.ejs",{listing}) 
 }))
-//route handler middleware 
- app.post("/listings",wrapAsync(async(req,res,next)=>{
-if(!req.body.listing) throw new ExpressError(400,"Invalid Listing Data")
+ //create listing
+ app.post("/listings",validateListing,wrapAsync(async(req,res,next)=>{
+
     const newlisting=new Listing(req.body.listing); 
     await newlisting.save();
     res.redirect("/listing")
@@ -62,10 +76,10 @@ if(!req.body.listing) throw new ExpressError(400,"Invalid Listing Data")
 
 
 //update route
-app.put("/listings/:id", wrapAsync(async(req,res)=>{
+app.put("/listings/:id",validateListing, wrapAsync(async(req,res)=>{
  let {id}=req.params;
- if(!req.body.listing) throw new ExpressError(400,"Invalid Listing Data");
     const listing=await Listing.findByIdAndUpdate(id,req.body.listing,{runValidators:true,new:true});
+   
     await listing.save();
     res.redirect(`/listings/${listing._id}`); 
 }))
