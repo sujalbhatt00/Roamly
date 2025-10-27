f3dffd38de7678720eab8e8d14b0ffd6c9eef53d
Sujal Bhatt <sujalbhatt500@gmail.com>
2025-10-27 17:59:42 +0530

    user.js changed

--- routes/user.js
+++ routes/user.js
@@ -8,10 +8,9 @@ const { cloudinary } = require("../cloudConfig.js");
 const Listing = require("../models/listing.js");
 const userController = require("../controller/users.js")
 const multer = require("multer");
-<<<<<<< HEAD
 
-=======
->>>>>>> my-fix-otp
+
+
 const { storage } = require("../cloudConfig.js");
 const upload = multer({
   storage,
